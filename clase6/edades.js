da229f68991cfc9b35d4936fb3c5d057f9442b9d
gallardodn <gallardodn@gmail.com>
2022-08-02 01:40:12 -0300

    Correccion de calculo mes actual

--- clase6/edades.js
+++ clase6/edades.js
@@ -1,13 +1,15 @@
+// Defino la fecha actual para futuras comparaciones.
 const fecha = new Date();
 const anioActual = fecha.getFullYear();
 const diaActual = fecha.getDate();
 const mesActual = fecha.getMonth() + 1;
+// inicializo variables globales del programa.
 let intentos = 0;
 let anioNacimiento=0;
 let mesNacimiento=0;
 let diaNacimiento=0;
 let repetir=false;
-let nombre=prompt("Por favor, Ingrese su nombre:")
+// creo la clase persona para futuras asignaciones.
 class persona {
     constructor (nombre,anioN,mesN,diaN,signo){
     this.nombre = nombre;
@@ -19,18 +21,17 @@ class persona {
     edad(){
         if ((this.diaN<=diaActual)&&(this.mesN<=mesActual)&&(this.anioN<=anioActual)) {
             return(this.nombre+" tiene una edad de: "+(anioActual-this.anioN)+" años "+(mesActual-this.mesN)+" mes/es y "+(diaActual-this.diaN)+" dias.");
-        } else if ((this.diaN>diaActual)&&(this.mesN<=mesActual)&&(this.anioN<=anioActual)) {
+        } else if ((this.diaN>diaActual)&&(this.mesN<mesActual)&&(this.anioN<=anioActual)) {
             return(this.nombre+" tiene una edad de: "+(anioActual-this.anioN)+" años "+(mesActual-this.mesN-1)+" mes/es y "+(diaActual-this.diaN+30)+" dias.");
         } else if ((this.diaN<=diaActual)&&(this.mesN>mesActual)&&(this.anioN<=anioActual)) {
             return(this.nombre+" tiene una edad de: "+(anioActual-this.anioN-1)+" años "+(mesActual-this.mesN+12)+" mes/es y "+(diaActual-this.diaN)+" dias.");
-        } else if ((this.diaN>diaActual)&&(this.mesN>mesActual)&&(this.anioN<=anioActual)) {
-            return(this.nombre+" tiene una edad de: "+(anioActual-this.anioN-1)+" años "+(mesActual-this.mesN+12)+" mes/es y "+(diaActual-this.diaN+30)+" dias.");
+        } else if ((this.diaN>diaActual)&&(this.mesN>=mesActual)&&(this.anioN<=anioActual)) {
+            return(this.nombre+" tiene una edad de: "+(anioActual-this.anioN-1)+" años "+(mesActual-this.mesN+11)+" mes/es y "+(diaActual-this.diaN+30)+" dias.");
         }
     }
 
 }
-const personas = [];
-personas.push(new persona("Miguel Ramirez",1981,7,21,"Cancer"));
+// Funcion que evalua el rango correcto ingresasdo por el usuario.
 function rangoincorrecto(ingresado,min,max){
     if ((ingresado<min)||(ingresado>max)) {
         return true;
@@ -38,6 +39,14 @@ function rangoincorrecto(ingresado,min,max){
         return false;
     }
 }
+
+// Creo un array y le cargo un objeto de tipo persona.
+const personas = [];
+personas.push(new persona("Miguel Ramirez",1981,7,21,"Cancer"));
+
+
+//Ingreso de datos del usuario.
+let nombre=prompt("Por favor, Ingrese su nombre:")
 do {
     anioNacimiento = parseInt(prompt("Ingrese su Año de Nacimiento:"));
     intentos = intentos +1;
@@ -62,6 +71,8 @@ if ((intentos<3)||(!repetir)) {
         if (repetir&&(intentos==3)) {alert("Supero el numero de intentos");}
         } while (repetir&&(intentos<3));
 }
+
+// Muestra de resultados
 if  ((intentos<3)||(!repetir)) {
     personas.push(new persona(nombre,anioNacimiento,mesNacimiento,diaNacimiento,"No Sabe aun"));
     for (const persona of personas){
